84c017050d0405452bc5f7647ff28985a8af7561
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2026-05-03 16:52:23 +0530

    knbmbmb

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  allowedDevOrigins: ["bedandbreakfast.manoramaseoservice.com"],
+  allowedDevOrigins: ["bedandbreakfast.manoramaseoservice.com","101.53.148.53"],
   images: {
     remotePatterns: [
       {
